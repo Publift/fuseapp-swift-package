d2197a4e8a841e544a44ee9545ff9fc5107d8a16
Nathan Peles <nathan@publift.com>
2026-02-10 08:33:47 +1100

    Upgrade GoogleMobileAds, FBAudienceNetwork, InMobi, Liftoff, PrebidMobile

--- Package.swift
+++ Package.swift
@@ -15,15 +15,15 @@ let package = Package(
     dependencies: [
         .package(
             url: "https://github.com/googleads/swift-package-manager-google-mobile-ads.git",
-            .upToNextMinor(from: "12.9.0")
+            .upToNextMinor(from: "12.14.0")
         ),
         .package(
             url: "https://github.com/googleads/swift-package-manager-google-user-messaging-platform.git",
             from: "3.0.0"
         ),
         .package(
-            url: "https://github.com/Vungle/VungleAdsSDK-SwiftPackageManager.git",
-            exact: "7.5.3"
+            url: "https://github.com/googleads/googleads-mobile-ios-mediation-liftoffmonetize.git",
+            exact: "7.6.0-release.0"
         )
     ],
     targets: [
@@ -32,13 +32,12 @@ let package = Package(
             dependencies: [
                 .product(name: "GoogleMobileAds", package: "swift-package-manager-google-mobile-ads"),
                 .product(name: "GoogleUserMessagingPlatform", package: "swift-package-manager-google-user-messaging-platform"),
-                .product(name: "VungleAdsSDK", package: "VungleAdsSDK-SwiftPackageManager"),
+                .product(name: "LiftoffMonetizeAdapter", package: "googleads-mobile-ios-mediation-liftoffmonetize"),
                 .target(name: "AmazonPublisherServicesSDK"),
                 .target(name: "FBAudienceNetwork"),
                 .target(name: "FuseAppSDKFramework"),
                 .target(name: "GoogleMobileAdsMediationFacebook"),
                 .target(name: "GoogleMobileAdsMediationInMobi"),
-                .target(name: "GoogleMobileAdsMediationVungle"),
                 .target(name: "InMobiSDK"),
                 .target(name: "PrebidMobile"),
                 .target(name: "PrebidMobileOMSDK"),
@@ -52,8 +51,8 @@ let package = Package(
         ),
         .binaryTarget(
             name: "FBAudienceNetwork",
-            url: "https://developers.facebook.com/resources/FBAudienceNetwork-6.20.1.zip",
-            checksum: "7e6560c585a8f224643500e89e053b909793a266ec483384d4c98215f0e870e4"
+            url: "https://developers.facebook.com/resources/FBAudienceNetwork-6.21.0.zip",
+            checksum: "de44e20e5f3e45753487f2e03beea385253573e024a4f2f24e18adfab35eca53"
         ),
         .binaryTarget(
             name: "FuseAppSDKFramework",
@@ -62,28 +61,23 @@ let package = Package(
         ),
         .binaryTarget(
             name: "GoogleMobileAdsMediationFacebook",
-            url: "https://dl.google.com/googleadmobadssdk/mediation/ios/meta/MetaAdapter-6.20.1.0.zip",
-            checksum: "3d1a8ca528d87aadf2124d46425848702c3194a71e9b738c452a84806bbae58a"
+            url: "https://dl.google.com/googleadmobadssdk/mediation/ios/meta/MetaAdapter-6.21.0.1.zip",
+            checksum: "f998ab78d31e7411b55cf319f74d5dbbcd623dff852a072fe7d11fed0fb13eda"
         ),
         .binaryTarget(
             name: "GoogleMobileAdsMediationInMobi",
-            url: "https://dl.google.com/googleadmobadssdk/mediation/ios/inmobi/InMobiAdapter-10.8.6.0.zip",
-            checksum: "3dcd1c6b2bc8ab0267642d3e9cc64be6f1cb8b353b64742effd56211d26d0450"
-        ),
-        .binaryTarget(
-            name: "GoogleMobileAdsMediationVungle",
-            url: "https://dl.google.com/googleadmobadssdk/mediation/ios/liftoffmonetize/LiftoffMonetizeAdapter-7.5.3.0.zip",
-            checksum: "d461bafea2b79e0505dae9773fc4ca4b3fc527c3bdcf98ceeaf844d0665fbedd"
+            url: "https://dl.google.com/googleadmobadssdk/mediation/ios/inmobi/InMobiAdapter-11.1.1.0.zip",
+            checksum: "e256fc5ec51499c54375404c5a048ac9ff3929fdbcc5b09f33a00ceeccc311b4"
         ),
         .binaryTarget(
             name: "InMobiSDK",
-            url: "https://dl.inmobi.com/inmobi-sdk/IM/InMobi-iOS-SDK-10.8.6.zip",
-            checksum: "ab0f05cd8aa0a7b1085a2b4f57f06ba27ae5dc310e1d9c1241011bba6ba98949"
+            url: "https://dl.inmobi.com/inmobi-sdk/IM/InMobi-iOS-SDK-11.1.1.zip",
+            checksum: "578dd32285cc8cea05e04ef3ffd03ccf0c93bc010d1e3abbed28690fe0dfffb2"
         ),
         .binaryTarget(
             name: "PrebidMobile",
-            url: "https://dl.cloudsmith.io/BrnMk9bbufLlX4Vd/publift/fuseapp/raw/files/PrebidMobile-iOS-3.1.0.zip",
-            checksum: "410c61af942550469ccc17ef748b9a2f4e5321094e18f910099f3d334db8391a"
+            url: "https://dl.cloudsmith.io/BrnMk9bbufLlX4Vd/publift/fuseapp/raw/files/PrebidMobile-iOS-3.2.1.zip",
+            checksum: "5dbfa93fd63b2a23f9456845810a4578c4d5a1f674fbd214e3d8f505ff14d40e"
         ),
         .binaryTarget(
             name: "PrebidMobileOMSDK",
